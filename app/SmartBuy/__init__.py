33cbb20eb95fc26f10a80ff9e39559e5c3b3efc5
Wei Xie <wei.tom.xie@gmail.com>
2021-02-02 11:04:38 -0800

    just need the data for a bit

--- app/SmartBuy/__init__.py
+++ app/SmartBuy/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 
-app = Flask(__name__)
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
 
 @app.route('/')
 def hello_world():
